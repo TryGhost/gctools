3a26acbb4ab2a46dfec03e0c1f94d286f15a29c1
Paul Davis <PaulAdamDavis@users.noreply.github.com>
2022-06-04 11:30:10 +0100

    Re-work content-stats tool

--- commands/content-stats.js
+++ commands/content-stats.js
@@ -1,16 +1,20 @@
-const contentStats = require('../tasks/content-stats');
+const GhostAdminAPI = require('@tryghost/admin-api');
 const ui = require('@tryghost/pretty-cli').ui;
+const Table = require('tty-table');
+const chalk = require('chalk');
+const siteCredentials = require('../lib/prompts/site-credentials.js');
+const {getStats} = require('../lib/stats.js');
 
 // Internal ID in case we need one.
 exports.id = 'content-stats';
 
-exports.group = 'Beta:';
+exports.group = 'Content:';
 
 // The command to run and any params
-exports.flags = 'content-stats <apiURL> <adminAPIKey>';
+exports.flags = 'content-stats [apiURL] [adminAPIKey]';
 
 // Description for the top level command
-exports.desc = 'See stats on how mich content your Ghost site has';
+exports.desc = 'See stats on how much content your Ghost site has';
 
 // Descriptions for the individual params
 exports.paramsDesc = [
@@ -20,6 +24,10 @@ exports.paramsDesc = [
 
 // Configure all the options
 exports.setup = (sywac) => {
+    sywac.boolean('-L --list', {
+        defaultValue: false,
+        desc: 'Choose from a list of saved sites, or add a new saved list'
+    });
     sywac.boolean('-V --verbose', {
         defaultValue: false,
         desc: 'Show verbose output'
@@ -28,23 +36,109 @@ exports.setup = (sywac) => {
 
 // What to do when this command is executed
 exports.run = async (argv) => {
-    let timer = Date.now();
-    let context = {errors: []};
+    // If no site url & key arr supplied
+    if (!argv.apiURL || !argv.adminAPIKey) {
+        let getSiteInfo = await siteCredentials.ask({
+            list: argv.list,
+            apiURL: argv.apiURL,
+            adminAPIKey: argv.adminAPIKey
+        });
 
-    try {
-        // Fetch the tasks, configured correctly according to the options passed in
-        let runner = contentStats.getTaskRunner(argv);
-
-        // Run the migration
-        await runner.run(context);
-
-        // Show the tables
-        ui.log(context.tables.stats);
-        ui.log(context.tables.users);
-
-        // Report success
-        ui.log.ok(`Successfully showed stats in ${Date.now() - timer}ms.`);
-    } catch (error) {
-        ui.log.error('There were errors', context.errors);
+        argv.apiURL = getSiteInfo.apiURL;
+        argv.adminAPIKey = getSiteInfo.adminAPIKey;
     }
+
+    // Init the API connection
+    argv.api = new GhostAdminAPI({
+        url: argv.apiURL,
+        key: argv.adminAPIKey,
+        version: 'v5.0'
+    });
+
+    // Get the content stats
+    const stats = await getStats({
+        api: argv.api
+    });
+
+    const statsHeader = [{
+        value: 'Resource',
+        headerColor: 'cyan',
+        color: 'white',
+        headerAlign: 'left',
+        align: 'left',
+        width: 20
+    },
+    {
+        value: 'Count',
+        headerColor: 'cyan',
+        color: 'white',
+        headerAlign: 'left',
+        align: 'left',
+        width: 10
+    }];
+
+    const statsRows = [
+        ['Posts', String(stats.posts.count)],
+        [chalk.gray('- Public'), chalk.gray(String(stats.public_posts.count))],
+        [chalk.gray('- Members'), chalk.gray(String(stats.members_posts.count))],
+        [chalk.gray('- Paid'), chalk.gray(String(stats.paid_posts.count))],
+        ['Pages', String(stats.pages.count)],
+        ['Tags', String(stats.tags.count)]
+    ];
+
+    ui.log(Table(statsHeader, statsRows, {compact: true}).render());
+
+    const membersHeader = [{
+        value: 'Members',
+        headerColor: 'cyan',
+        color: 'white',
+        headerAlign: 'left',
+        align: 'left',
+        width: 20
+    },
+    {
+        value: 'Count',
+        headerColor: 'cyan',
+        color: 'white',
+        headerAlign: 'left',
+        align: 'left',
+        width: 10
+    }];
+
+    const membersRows = [
+        ['Total', String(stats.members.count)],
+        [chalk.gray('- Free'), chalk.gray(String(stats.members.status.free.count))],
+        [chalk.gray('- Complimentary'), chalk.gray(String(stats.members.status.comped.count))],
+        [chalk.gray('- Paid'), chalk.gray(String(stats.members.status.paid.count))]
+    ];
+
+    ui.log(Table(membersHeader, membersRows, {compact: true}).render());
+
+    const usersHeader = [{
+        value: 'Staff',
+        headerColor: 'cyan',
+        color: 'white',
+        headerAlign: 'left',
+        align: 'left',
+        width: 20
+    },
+    {
+        value: 'Count',
+        headerColor: 'cyan',
+        color: 'white',
+        headerAlign: 'left',
+        align: 'left',
+        width: 10
+    }];
+
+    const usersRows = [
+        ['Total', String(stats.users.count)],
+        [chalk.gray('- Owner'), chalk.gray(String(stats.users.roles.owner.count))],
+        [chalk.gray('- Administrator'), chalk.gray(String(stats.users.roles.administrator.count))],
+        [chalk.gray('- Editor'), chalk.gray(String(stats.users.roles.editor.count))],
+        [chalk.gray('- Author'), chalk.gray(String(stats.users.roles.author.count))],
+        [chalk.gray('- Contributor'), chalk.gray(String(stats.users.roles.contributor.count))]
+    ];
+
+    ui.log(Table(usersHeader, usersRows, {compact: true}).render());
 };
