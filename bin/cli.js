506176fb3ba6f4193a97f7e32056a29b1c2bf277
Nick Moreton <nick@ghost.org>
2025-06-12 13:30:46 +0100

    Added clean slugs tool

--- bin/cli.js
+++ bin/cli.js
@@ -39,6 +39,7 @@ import getPosts from '../commands/get-posts.js';
 import setTemplate from '../commands/set-template.js';
 import addMemberCompFromCSVCommands from '../commands/add-member-comp-from-csv.js';
 import setFeaturedImages from '../commands/set-featured-images.js';
+import cleanSlugs from '../commands/clean-slugs.js';
 
 prettyCLI.command(addMemberCompSubscriptionCommands);
 prettyCLI.command(removeMemberCompSubscriptionCommands);
@@ -76,6 +77,7 @@ prettyCLI.command(getPosts);
 prettyCLI.command(setTemplate);
 prettyCLI.command(addMemberCompFromCSVCommands);
 prettyCLI.command(setFeaturedImages);
+prettyCLI.command(cleanSlugs);
 
 prettyCLI.style({
     usageCommandPlaceholder: () => '<source or utility>'
